3e0f6f425dffc6ec516f466340ae6ffb3ecd28ab
Aizher800 <Aizher800@users.noreply.github.com>
2023-09-17 11:52:52 +0800

    Unity update
    more code stuff

--- Assets/Script/LoadScene.cs
+++ Assets/Script/LoadScene.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    public GameObject LoseScreen;
+
+    public void LoseGame()
+    {
+        LoseScreen.SetActive(true);
+        StartCoroutine(DelayLoseLoad());
+        Debug.Log("You've pressed the restart button");
+        Debug.Log("Please wait for 3 seconds");
+    }
+
+    IEnumerator DelayLoseLoad()
+    {
+        yield return new WaitForSeconds(3f);
+        Restart();
+    }
     //Loads scene from Home to MainGame
     public void SceneLoader(string sceneName)
     {
@@ -12,7 +28,7 @@ public class LoadScene : MonoBehaviour
     
     }
 
-    public void Restart()
+    private void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Debug.Log("Yo. You've restarted the game, dawg.");
